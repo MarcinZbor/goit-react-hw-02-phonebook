7366827dca7587db5e4048bb7e2fdcfb0b478854
MarcinZbor <marcinzb@gazeta.pl>
2023-05-16 19:04:14 +0200

    chaningfromhw03tohw02

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,66 @@
 import React from 'react';
+import { nanoid } from 'nanoid';
+import ContactForm from './ContactForm/ContactForm';
+import ContactsList from './ContactList/ContactList';
+import Filter from 'components/Filter/Filter';
+import styles from './App.module.css';
 
+ class App extends React.Component {
+  state = {
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
+  };
 
-const App = () => {
-  return (
-    <>
-    Welcome 2
-    </>
-  );
-};
+  handleSubmit = event => {
+    event.preventDefault();
+    const name = event.currentTarget.elements.name.value;
+    const number = event.currentTarget.elements.number.value;
+    const newContact = { name: name, id: nanoid(), number: number };
+    this.state.contacts.some(contact => name === contact.name)
+      ? alert(`${name} is already in contacts.`)
+      : this.setState(prevState => ({
+          contacts: [...prevState.contacts, newContact],
+        }));
+    event.currentTarget.elements.name.value = '';
+    event.currentTarget.elements.number.value = '';
+  };
 
-export default App;
+  handleSearch = event => {
+    this.setState({ filter: event.currentTarget.value });
+  };
+
+  handleFilter = () =>
+    this.state.filter
+      ? this.state.contacts.filter(contact =>
+          contact.name.toLowerCase().includes(this.state.filter.toLowerCase())
+        )
+      : this.state.contacts;
+
+  handleDelete = id => {
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(contact => contact.id !== id),
+    }));
+  };
+
+  render() {
+    return (
+      <div className={styles.container}>
+        <h1 className={styles.title}>Phonebook</h1>
+        <ContactForm   formSubmit={this.handleSubmit} />
+        <h2 className={styles.title}>Contacts</h2>
+        <Filter inputSearch={this.handleSearch} />
+        <ContactsList
+          contacts={this.handleFilter()}
+          onDelete={this.handleDelete}
+        />
+      </div>
+    );
+  }
+}
+
+export default App
